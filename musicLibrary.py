2b87812b76671c779e45c46297f4ddfb22824caa
Shubhranshu Ranasingh <166228058+DuduinCode@users.noreply.github.com>
2024-08-04 18:40:50 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music={
+    "bye": "https://www.youtube.com/watch?v=C27NShgTQE4&list=RDC27NShgTQE4&start_radio=1",
+    "tumse":"https://www.youtube.com/watch?v=Nnop2walGmM",
+    "tauba":"https://www.youtube.com/watch?v=LK7-_dgAVQE&list=RDC27NShgTQE4&index=8"
+}
