63d583a4194d0ca2fed4c64fd6597f6bb5642378
Shubhranshu Ranasingh <166228058+DuduinCode@users.noreply.github.com>
2024-08-26 11:59:39 +0530

    Update AI.py

--- AI.py
+++ AI.py
@@ -1,17 +1,8 @@
-from openai import OpenAI
- 
-# pip install openai 
-# if you saved the key under a different environment variable name, you can do something like:
-AI= OpenAI(
-  api_key="<your openai api key>",
-)
-
-completion = AI.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a virtual assistant named friday skilled in general tasks like Alexa and Google Cloud"},
-    {"role": "user", "content": "what is coding"}
-  ]
-)
-
-print(completion.choices[0].message.content)
+def aiProcess(command):
+    chat_session = genai.GenerativeModel(
+        model_name="gemini-1.5-flash",
+        generation_config=generation_config,
+    ).start_chat(history=[])
+    
+    response = chat_session.send_message(command)
+    return response.text
